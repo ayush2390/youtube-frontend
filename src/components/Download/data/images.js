0f7804532d328255bb4d859e423b31d3c1a94a43
ayush2390 <tsayush2390@gmail.com>
2024-11-18 12:55:37 +0530

    first commit

--- src/components/Download/data/images.js
+++ src/components/Download/data/images.js
@@ -1,11 +1,11 @@
 import image0 from "../images/image0.png";
 import Search from "../images/Search.png";
-import Play from "../images/Play.png";
+import play from "../images/play.png";
 import logo from "../images/logo.png";
 const images = {
   videoPreview: `${image0}`,
   Search: `${Search}`,
-  Play: `${Play}`,
+  Play: `${play}`,
   logo: `${logo}`,
 };
 export default images;
